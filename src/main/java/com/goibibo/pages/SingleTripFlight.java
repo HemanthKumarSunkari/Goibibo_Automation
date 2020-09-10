bc27e01b84857239291c8075dc3a8cd6b951ce40
HemanthKumarSunkari <hemanth.monk@gmail.com>
2020-09-10 11:39:23 +0530

    Updated the script to find the shortest duration amoung all flights in the list

--- src/main/java/com/goibibo/pages/SingleTripFlight.java
+++ src/main/java/com/goibibo/pages/SingleTripFlight.java
@@ -29,6 +29,9 @@ public class SingleTripFlight extends TestBase {
 	@FindBy(xpath = "//*[contains(@data-cy,'bookBtn')]")
 	List<WebElement> bookBtn;
 
+	
+	/********  Action Methods  *******/
+	int index = 0;
 	public void turnPriceToggleUp() {
 		String clName = price.getAttribute("class");
 		if (!clName.contains("icon-arrow2-up")) {
@@ -42,46 +45,47 @@ public class SingleTripFlight extends TestBase {
 	public int findCheapFlightFare() {
 		int len = fares.size();
 		int min = 0, max = 0;
-
+		min = Integer.parseInt(fares.get(0).getText().replaceAll(",", ""));
 		for (int i = 1; i < len; i++) {
-			min = Integer.parseInt(fares.get(i - 1).getText().replaceAll(",", ""));
+			
 			max = Integer.parseInt(fares.get(i).getText().replaceAll(",", ""));
 			
 			if (max < min) {
-				int temp = min;
 				min = max;
-				max = temp;
+				index = i;
 			}
 		}
 
 		return min;
 	}
 
-	public int findShortestFlightDuration() {
+	public String findShortestFlightDuration() {
 
 		int len = duration.size();
 		int maxD, minD = 0;
-
-		for (int i = 0; i < len; i++) {
-			minD = Integer.parseInt(duration.get(i).toString());
-			maxD = Integer.parseInt(duration.get(i++).toString());
+		minD = Integer.parseInt(duration.get(0).getText().replaceAll(" ", "").replaceAll("h", "").replaceAll("m", ""));
+				
+		for (int i = 1; i < len; i++) {
+			maxD = Integer.parseInt(duration.get(i).getText().replaceAll(" ", "").replaceAll("h", "").replaceAll("m", ""));
 			if (maxD < minD) {
-				int temp = minD;
 				minD = maxD;
-				maxD = temp;
 			}
 		}
-
-		return minD;
+		
+		int hour = minD/100;
+		int minute = minD - (hour*100);
+		return hour+"hr "+minute+"min";
 	}
 
 	public flightReviewPage selectLowFareFlight() {
 
 		turnPriceToggleUp();
+		
 
-		System.out.println("Cheapest fare of among all Flight = ₹" + findCheapFlightFare());
-		System.out.println("Shortest duration among all flights = " + duration.get(0).getText());
-		bookBtn.get(0).click();
+		System.out.println("Cheapest fare of among all Flight = ₹"+ findCheapFlightFare());
+		System.out.println("Shortest duration among all flights = " + findShortestFlightDuration());
+		System.out.println("Index of selected flight = " + index);
+		bookBtn.get(index).click();
 
 		return new flightReviewPage();
 
